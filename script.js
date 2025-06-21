369a444111c457bc7007143e190ae618ee6f070a
Brinly jaja <belemajaja3@gmail.com>
2025-06-21 18:59:08 +0100

    update

--- script.js
+++ script.js
@@ -99,3 +99,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const cursor = document.querySelector('.custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
+
